ce32589459151419a43743b65bad941ecac42590
silvajpedro <silvajpro.31@gmail.com>
2023-08-18 20:04:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+let contador = 0
+
+// console.log(contador + 1)
+// console.log(contador + 1)
+// console.log(contador + 1)
+// console.log(contador + 1)
+// console.log(contador + 1)
+
+// console.log(contador + 1 +1)
+
+
+// OPERADORES DE ATRIBUIÇÃO
+
+// atribução de adição
+// forma completa 
+//                        0     + 1
+console.log(contador = contador + 1)
+
+//                        1     + 1
+console.log(contador = contador + 1)
+
+//                        2     +  1
+console.log(contador = contador + 1)
+
+
+// forma simplificada
+console.log(contador +=1)
+
+console.log(contador +=1)
+
+// atribução de subtração
+
+console.log(contador -= 1)
+
+console.log(contador -= 2)
+
+// atribuição de multiplicação
+
+console.log(contador *= 3)
+
+console.log(contador *= 2)
+
+// atribuição de divisão
+
+console.log(contador /= 4)
+
+console.log(contador /= 2.5)
+
+// console.log(contador *= 3)
+
+// console.log(`A divisao por 3 seria de ${contador /= 3}`)
+
+let segundoContador = 0
+
+console.log(segundoContador += 1)
+
+console.log(segundoContador += 1)
+
+console.log(segundoContador+= 1)
+
+console.log(segundoContador+=1)
+
+// LAÇOS DE REPETIÇÃO / LOOPS
+// Estrutura de código, que irá executar uma ação repetidas vezes, até que que a condição seja falsa
+
+// diferente do if/else, o loop executá várias vezes a ação, enquanto o if/else executa apenas 1 vez.
+
+// enquanto
+// while(){}
+
+// while(vai ser a nossa condição){
+//     será a nossa ação
+// }
+
+let terceiroContador = 0
+
+while(true){
+    console.log(terceiroContador+=1)
+}
+
+// para a variável contador = 0, 
+// enquanto ela for menor ou igual a 500
+// faça contador + 1
+
+// for(let contador = 0; contador <= 500; contador+=1){
+//     console.log(contador)
+// }
+
+
+
+
+
+
+
+
+// do{} while()
